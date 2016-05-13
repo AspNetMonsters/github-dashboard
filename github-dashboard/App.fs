e094fc0f8d646bc95b872a2592d28efb5d1ad24b
Simon Timms <stimms@gmail.com>
2016-05-13 09:54:29 -0600

    Trying to get octokit working

---
+++ github-dashboard/App.fs
@@ -0,0 +1,22 @@
+﻿namespace dashboard
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.Files
+
+module entry =
+
+    let router = 
+        choose [
+            path "/" >=> (OK "Home")
+            path "/git" >=> (GitHub.list)
+            pathRegex "(.*)\.js" >=> Files.browseHome
+        ]
+
+    [<EntryPoint>]
+    let start(args) = 
+        startWebServer defaultConfig router
+        0
+    
---
+++ github-dashboard/GitHub.fs
@@ -0,0 +1,15 @@
+﻿namespace dashboard
+
+open Suave
+open Suave.Successful
+open Octokit
+
+module GitHub =
+
+    let list = 
+            let client = new GitHubClient(new ProductHeaderValue("github-dashboard"))
+            let user = client.User.Get("stimms").Result
+            OK user.Bio
+        
+  
+        
