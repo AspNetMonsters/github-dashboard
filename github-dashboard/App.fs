e7e7cd74d178e694a9e1b6e36bc221f4a5e58b90
Simon Timms <stimms@gmail.com>
2016-05-13 10:48:00 -0600

    Returning json

--- github-dashboard/App.fs
+++ github-dashboard/App.fs
@@ -1,8 +1,9 @@
-﻿namespace dashboard
+﻿
 
 open Suave
 open Suave.Filters
 open Suave.Operators
+open Suave.Writers
 open Suave.Successful
 open Suave.Files
 
@@ -11,7 +12,7 @@ module entry =
     let router = 
         choose [
             path "/" >=> (OK "Home")
-            path "/git" >=> (GitHub.list)
+            pathScan "/git/%s" ( fun name -> setHeader "Content-Type" "application/json" >=> GitHub.list(name)  )
             pathRegex "(.*)\.js" >=> Files.browseHome
         ]
 
--- github-dashboard/GitHub.fs
+++ github-dashboard/GitHub.fs
@@ -1,15 +1,24 @@
-﻿namespace dashboard
+﻿module GitHub 
 
+open Microsoft.FSharp.Collections
 open Suave
 open Suave.Successful
+open Suave.Filters
+open Suave.Writers
 open Octokit
+open Newtonsoft
 
-module GitHub =
+    type IssueRecord = { id: int; name: string; url: string}
 
-    let list = 
+    let list(name) = 
+            
             let client = new GitHubClient(new ProductHeaderValue("github-dashboard"))
-            let user = client.User.Get("stimms").Result
-            OK user.Bio
+            let issues = client.PullRequest.GetAllForRepository("HTBox", "allReady").Result
+            let listOfIssues = List.ofSeq(issues)
+            let records =  listOfIssues |> List.map(fun iss -> { id = iss.Number; name= iss.Title; url= iss.Url.ToString() })
+            
+            OK (Newtonsoft.Json.JsonConvert.SerializeObject records)
+            
         
   
         
